487cf186d9fb7154c863602d1fa36dc9aeea2dee
sakshi-1505 <sakshipatle.0808cs171134@gmail.com>
2024-01-02 01:21:22 +0530

    feat: add mock go server for repo1
    Signed-off-by: sakshi-1505 <sakshipatle.0808cs171134@gmail.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module repo-1
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World from Repo-1!")
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+
+	addr := ":8080"
+
+	log.Printf("Server starting on %s", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatal("Server failed to start:", err)
+	}
+}
